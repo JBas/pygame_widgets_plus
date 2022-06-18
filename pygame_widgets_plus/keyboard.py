ba8e27c9c4f0963b2e06bab5e38ae72fcc5000b6
Joshua Bas <joshua.n.bas@gmail.com>
2022-06-18 14:32:20 +0000

    implemented thread locks when accessing keyboard buffer

--- pygame_widgets_plus/keyboard.py
+++ pygame_widgets_plus/keyboard.py
@@ -1,9 +1,11 @@
 import pygame
+from threading import Lock
 
 class meta_KeyboardHandler(type):
     def __init__(cls, *args, **kwargs):
         cls._buffer = ""
-
+        cls._lock = Lock()
+    
     @property
     def buffer(cls):
         return cls._buffer
@@ -14,21 +16,29 @@ class meta_KeyboardHandler(type):
 
 class KeyboardHandler(metaclass=meta_KeyboardHandler):
 
-    # not thread-safe
     def consume():
+        
+        KeyboardHandler._lock.acquire()
+        
         if (len(KeyboardHandler.buffer) <= 0):
+            KeyboardHandler._lock.release()
             return ""
             
         c = KeyboardHandler.buffer[0]
         KeyboardHandler.buffer = KeyboardHandler.buffer[1:]
+        
+        KeyboardHandler._lock.release()
+        
         return c
 
-    # not thread-safe
     def flush():
+        KeyboardHandler._lock.acquire()
         KeyboardHandler.buffer = ""
+        KeyboardHandler._lock.release()
         
     def update(event):
         if (event.type == pygame.KEYDOWN):
-            
+            KeyboardHandler._lock.acquire()
             KeyboardHandler.buffer += event.unicode
+            KeyboardHandler._lock.release()
         pass
